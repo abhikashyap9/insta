49804e4a7127b4e2146463a45d3a375bdbf48823
Rakshit Mehra <rakshitmehra15@gmail.com>
2022-12-28 01:17:07 +0530

    making api for saved messages

--- server/index.ts
+++ server/index.ts
@@ -1,49 +1,46 @@
-import app from './app'
-import http from 'http'
-import config from './utils/config'
-import logger from './utils/logger'
-import initMongodb from './utils/initMongodb'
-const server = http.createServer(app)
-import {Server} from "socket.io"
-
+import app from "./app";
+import http from "http";
+import config from "./utils/config";
+import logger from "./utils/logger";
+import initMongodb from "./utils/initMongodb";
+const server = http.createServer(app);
+import { Server } from "socket.io";
 
 const io = new Server(server, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-    },
-  });
-console.log('ax',server);
-  
-  
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+console.log("ax", server);
+
 async function main() {
-	await initMongodb.connect()
+  await initMongodb.connect();
 
-	server.listen(config.PORT, () => {
-		logger.info(`Server rununing on port ${config.PORT}`)
-	})
+  server.listen(config.PORT, () => {
+    logger.info(`Server rununing on port ${config.PORT}`);
+  });
 }
-main()
-  io.on("connection",(socket:any)=>{
-          console.log('Connection Established')
-        
-          socket.on("join_room", (data:any) => {
-            socket.join(data);
-            console.log(`User with ID: ${socket.id} joined room: ${data}`);
-          });
+main();
+io.on("connection", (socket: any) => {
+  console.log("Connection Established");
 
-          socket.on('ping', (data:any) => {
-            console.log('data',data)
-            socket.to(data.room).emit("receive_message", data);
-            console.log(data)
-            });
+  socket.on("join_room", (data: any) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
 
-            socket.on("disconnect", () => {
-              console.log("User Disconnected", socket.id);
-            });
+  socket.on("ping", (data: any) => {
+    console.log("data", data);
+    socket.to(data.room).emit("receive_message", data);
+    console.log(data);
+  });
 
-      })
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 // const server = http.createServer(app)
-	
+
 // mongodb+srv://abhishek:<password>@insta.bpmz0tv.mongodb.net/?retryWrites=true&w=majority
--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,15 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 const config = {
-	// verbose: true,
-	testEnvironment: 'node',
-	preset: 'ts-jest',
-	testPathIgnorePatterns: ['/node_modules/', '/utils'],
+  //   verbose: true,
+  testEnvironment: "node",
+  preset: "ts-jest",
+  testPathIgnorePatterns: ["/node_modules/", "/utils"],
 
-	compilerOptions: {
-		// ... rest
-		"typeRoots": ["./node_modules/@types", "./typings"]
-	}
+  compilerOptions: {
+    // ... rest
+    typeRoots: ["./node_modules/@types", "./typings"],
+  },
+};
 
-}
-
-export default config
+export default config;
