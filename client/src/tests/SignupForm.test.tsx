c4bd8f09e560efffb053cff40ba883c8552aadd4
= <=>
2024-05-31 22:20:14 +0530

    Update/Test integrated

--- client/src/tests/SignupForm.test.tsx
+++ client/src/tests/SignupForm.test.tsx
@@ -2,6 +2,8 @@ import { render, screen } from '@testing-library/react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
 import Signup from '../LoginAuth/Signup'
+import userEvent from "@testing-library/user-event"
+
 const queryClient = new QueryClient()
 
 describe('Signup From', () => {
@@ -14,13 +16,30 @@ describe('Signup From', () => {
             </QueryClientProvider>
         )
 
-        const email= screen.getAllByPlaceholderText(/Enter your email/i)
-        const fullName= screen.getAllByPlaceholderText(/Enter your full name/i)
-        const userName= screen.getAllByPlaceholderText(/Enter Your User Name/i)
-        const password= screen.getAllByPlaceholderText(/Password/i)
+        const email= screen.getByPlaceholderText(/Enter your email/i)
+        const fullName= screen.getByPlaceholderText(/Enter your full name/i)
+        const userName= screen.getByPlaceholderText(/Enter Your User Name/i)
+        const password= screen.getByPlaceholderText(/Password/i)
     
-        expect(email)
+        expect(email).toBeInTheDocument()
+        expect(fullName).toBeInTheDocument()
+        expect(userName).toBeInTheDocument()
+        expect(password).toBeInTheDocument()
     })
 
+
+    it('Test some outputs',()=>{
+        render(
+            <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
+                    <Signup />
+                </BrowserRouter>
+            </QueryClientProvider>
+        )
+
+        const signUpButton= screen.queryByRole('button',{name:'Signup'})
+        const user=userEvent.setup()
+        await user.click(showMoreButton)
+    })
 
  })
